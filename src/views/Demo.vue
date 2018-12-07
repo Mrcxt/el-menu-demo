be5fcb3efd2f035ea2178ae6100c36db63aeeef5
mrcxt <847428891@qq.com>
2018-12-07 10:44:46 +0800

    demo

--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -16,68 +16,61 @@
               </div>
               <pre class="language-js">
                 <code>
-                  [
-                    {
-                        "path": "/menu_0",
-                        "name": "menu_0",
-                        "meta": {
-                            "visible": true
-                        }
-                    },
-                    {
-                        "path": "/menu_1",
-                        "name": "menu_1",
-                        "meta": {
-                            "visible": true
-                        }
-                    },
-                    {
-                        "path": "/menu_2",
-                        "name": "menu_2",
-                        "meta": {
-                            "visible": true
-                        }
-                    },
-                    {
-                        "path": "/menu_3",
-                        "name": "menu_3",
-                        "meta": {
-                            "visible": true
-                        }
-                    },
-                    {
-                        "path": "/menu_4",
-                        "name": "menu_4",
-                        "meta": {
-                            "visible": true
-                        },
-                        "children": [
-                            {
-                                "path": "menu_4_1",
-                                "name": "menu_4_1",
-                                "meta": {
-                                    "visible": true
-                                }
-                            },
-                            {
-                                "path": "menu_4_2",
-                                "name": "menu_4_2",
-                                "meta": {
-                                    "visible": true
-                                },
-                                "children": [
-                                    {
-                                        "path": "menu_4_2_1",
-                                        "name": "menu_4_2_1",
-                                        "meta": {
-                                            "visible": true
-                                        }
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
+export default new Router({
+  routes: [{
+          path: '/menu_0',
+          name: 'menu_0',
+          meta: {
+              visible: true
+          }
+      }, {
+          path: '/menu_1',
+          name: 'menu_1',
+          meta: {
+              visible: true
+          }
+      }, {
+          path: '/menu_2',
+          name: 'menu_2',
+          meta: {
+              visible: true
+          }
+      }, {
+          path: '/menu_3',
+          name: 'menu_3',
+          meta: {
+              visible: true
+          }
+      },
+      {
+          path: '/menu_4',
+          name: 'menu_4',
+          meta: {
+              visible: true,
+          },
+          children: [{
+              path: 'menu_4_1',
+              name: 'menu_4_1',
+              meta: {
+                  visible: true,
+              },
+          }, {
+              path: 'menu_4_2',
+              name: 'menu_4_2',
+              meta: {
+                  visible: true,
+              },
+              children: [{
+                  path: 'menu_4_2_1',
+                  name: 'menu_4_2_1',
+                  meta: {
+                      visible: true,
+                  },
+              }]
+          }, ]
+      },
+  ]
+})
                 </code>
               </pre>
             </el-card>
